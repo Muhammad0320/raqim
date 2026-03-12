a634632c5d3ebd0051d90d62801c89ea35b6e615
muhammad0320 <balogunmuhammadawwal@gmail.com>
2026-03-12 11:25:53 +0100

    chore: add synapse cli proj

--- synapse-core/src/lancedb_store.rs
+++ synapse-core/src/lancedb_store.rs
@@ -97,9 +97,9 @@ impl LanceEngine {
         .expect("Failed to build Arrow RecordBatch");
 
         // 4. Commit to the DB
-        let result = std::result::Result<RecordBatch, arrow_schema::ArrowError> = std::result::Result::Ok(batch);
-        let batches =
-            RecordBatchIterator::new(vec![result], self.schema());
+        let result: std::result::Result<RecordBatch, arrow_schema::ArrowError> =
+            std::result::Result::Ok(batch);
+        let batches = RecordBatchIterator::new(vec![result], self.schema());
 
         let table_names = self.db.table_names().execute().await.unwrap();
         if table_names.contains(&self.table_name) {
