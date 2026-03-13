dfdb27ba310acd46504ed50e7165b8d2da84aa15
muhammad0320 <balogunmuhammadawwal@gmail.com>
2026-03-14 00:04:29 +0100

    chore: add synapse cli proj

--- rqm-tui/src/main.rs
+++ rqm-tui/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::*,
+    widgets::{Block, Borders, Paragraph},
+};
+use std::io::{stdout, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    println!("Bismillah. Booting RQM Mission Control...");
+
+    // Setup Terminal for TUI
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    // Main UI loop 
+
+}
+
+fn ui(f: &mut Frame) {
+
+    // 1. Split the screen horizontally into right and left column
+    
+
 }
