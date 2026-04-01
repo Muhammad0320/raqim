00bc82b6f4b12d022fb89f124cfbb07a1b8796bc
muhammad0320 <balogunmuhammadawwal@gmail.com>
2026-04-01 09:12:39 +0100

    chore: update mcp

--- raqim-agent-sdk/src/lib.rs
+++ raqim-agent-sdk/src/lib.rs
@@ -4,13 +4,16 @@
 extern "C" {
 
     host_emit_thought(ptr: *const u8, len: usize);
+    fn host_register_capability(ptr: *const u8, len: usize);
     fn host_get_time() -> i64;
+    fn host_request_entropy() -> u64;
     fn host_ask_agent(
         cap_ptr: *const u8, cap_len: usize, 
         payload_ptr: *const u8, payload_len: usize,
         out_ptr: *mut u8, max_len: usize
-    ) -> 132;
-    fn host_register_capability(ptr: *const u8, len: usize);
+    ) -> i32;
+
+    fn host_fetch_url(url_ptr: *const u8, url_len: usize, out_ptr: *mut u8, out_len: usize ) -> i32;
 
 }
 
@@ -39,8 +42,6 @@ impl Raqim {
         if required_size == 0 {
             return Ok(Vec::new())
         }
-
-        
         
         // PASS 2: Allocate exactly what is needed and fetch the data
         let mut res_buffer = vec![0u8; required_size as usize];
@@ -61,7 +62,37 @@ impl Raqim {
         Ok(res_buffer)
     }
 
-    pub fn emit_thought(state: &AgentState) {
+
+    pub fn fetch_url(url: &str) -> Result<String, String> {
+
+        // First pass
+        let required_size = unsafe {
+            host_fetch_url(url.as_ptr(), url.len(), std::ptr::null_mut, 0)
+        }
+
+        if required_size < 0 {
+            return Err("Network request failed".to_string());
+        }
+
+        if required_size == 0 {
+            return Ok("".to_string())
+        }
+
+        // Second pass
+        let mut res_buffer = vec![0u8; required_size as usize];
+        let bytes_written = unsafe {
+            host_fetch_url(url.as_ptr(), url.len(), res_buffer.as_mut_ptr(), res_buffer.len())
+        }
+
+        if bytes_written != required_size {
+            return Err("Memory mismatch during 2-pass allocation".to_string());
+        }
+
+        String::from_utf8(res_buffer).map_err(|e| e.to_string())
+    }
+
+
+        pub fn emit_thought(state: &AgentState) {
         let bytes = rkyv::to_bytes::<rkyv::rancor::Error>(state);
         unsafe {
             host_emit_thought(bytes.as_ptr(), bytes.len());
@@ -77,5 +108,6 @@ impl Raqim {
         }
 
     }
+
 
 }
