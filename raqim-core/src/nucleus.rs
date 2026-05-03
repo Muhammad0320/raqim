206cc47837ae27f629e6295064fa544305a5ac2c
muhammad0320 <balogunmuhammadawwal@gmail.com>
2026-05-03 02:17:50 +0100

    chore: update

--- raqim-core/src/nucleus.rs
+++ raqim-core/src/nucleus.rs
@@ -107,7 +107,7 @@ impl WalEngine {
     pub async fn append(&self, log: OpLog) {
         let _ = self.sender.send(log).await;
     }
-    
+
     /// Extremely fast binary scan of the active WAL for a specific substring
     pub fn lexical_scan(
         &self,
@@ -120,7 +120,7 @@ impl WalEngine {
 
         // Page the WAL directly into virtual memory. Zero read() syscall overhead
         let mmap = unsafe { MmapOptions::new().map(&file)? };
-         
+
         // Compile the search automaton (case-insensitive)
         let ac = AhoCorasick::builder()
             .ascii_case_insensitive(true)
@@ -170,7 +170,7 @@ impl WalEngine {
     }
 
     /// Returns the exact number of uncompacted thoughts currently in the Hot WAL. O(1) operation utilizing the BTreeMap index
-    pub fn get_pending_count(&self) usize {
+    pub fn get_pending_count(&self) -> usize {
         self.index.read().unwrap().len()
     }
 
@@ -213,5 +213,4 @@ impl WalEngine {
 
         highest_tx
     }
-
 }
