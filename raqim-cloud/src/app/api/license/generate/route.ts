e1ac53a9fa4520d84d5ec7b32dfdf567b3c09e39
muhammad0320 <balogunmuhammadawwal@gmail.com>
2026-05-08 18:27:00 +0100

    chore: update

---
+++ raqim-cloud/src/app/api/license/generate/route.ts
@@ -0,0 +1,19 @@
+import {SignJwt, importPKCS8} from "jose";
+import { NextResponse } from "next/server";
+import {supabaseAdmin} from "@/lib/supabase"
+
+export async function POST(request:Request) {
+        const {org_id, requested_features} = await request.json();
+        const features = new Set(requested_features)
+        // Fetch the Private Key from Supabase Vault (or Vercel Env)
+        const privateKeyPem = process.env.RAQIM_RSA_PRIVATE_KEY;
+        if (!privateKeyPem) throw new Error("KMS offline");
+
+        // Cryptographic ignition. 
+        const private_key = await importPKCS8(privateKeyPem, "RS256");
+
+        const jwt = await new SignJwt({features: Array.from(features)}).setProtectedHeaderr({alg: "RS256"}).setSubject(org_id).setIssueAt().SetExpirationTime("7d").sign(private_key);
+
+        // Update the DB
+        await supabaseAdmin.from("licenses").update({ jwt_hash:  await hash(jwt) })
+}
