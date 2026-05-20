c41e82254d9c0ca63bc4ed2f097018c20af5de12
muhammad0320 <balogunmuhammadawwal@gmail.com>
2026-05-20 08:52:26 +0100

    chore: updates

---
+++ raqim-siege/src/lib.rs
@@ -0,0 +1,27 @@
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+pub struct IngressEnvelope {
+    pub intent_path: String,  // "raqim_finance/ledger" ( Checked by Aegis )
+    pub public_key: [u8; 32], // The Ed25519 public key of the sender
+    pub signature: [u8; 64],  // The mathematical signauture proving authenticity
+    pub state_bytes: Vec<u8>, // The actual thought
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq, Clone)]
+pub struct AgentState {
+    pub agent_id: Option<[u8; 16]>,
+    pub transaction_id: u64,
+
+    pub timestamp: i64,
+    pub status: AgentStatus,
+
+    pub text: String,
+    pub namespace: String,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq, Clone)]
+pub enum AgentStatus {
+    Idle,
+    Reasoning,     // Waiting on LLM token generation
+    ToolExecution, // Executing an external API or tool
+    Halted,        // Interdicted by the Aegis security layer
+}
